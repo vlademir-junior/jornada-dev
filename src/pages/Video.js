b1198998fb6632c88f6fdf40d49379ae3fee3f61
vlademir-junior <vlademir.junior.nh@gmail.com.br>
2023-06-16 08:05:57 -0300

    finalizando segunda aula

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,37 +1,31 @@
-import React, { useRef, useState } from 'react'
-import "./Video.css"
+import React, { useRef, useState } from "react";
+import "./Video.css";
 
 function Video() {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
 
-  const videoRef = useRef(null)
-  const [play, setPlay] = useState(false)
-
-    function handleStart() {
-
-      if (play) {
-        videoRef.current.pause()
-        setPlay(false)
-      } else {
-        videoRef.current.play()
-        setPlay(true)
-      }
-
+  function handleStart() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
     }
+  }
 
   return (
-    <div className='video'>
-
+    <div className="video">
       <video
-          className='video__player'
-          ref={videoRef}
-          onClick={handleStart}
-          loop
-          src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
-        >
-      </video>
-
+        className="video__player"
+        ref={videoRef}
+        onClick={handleStart}
+        loop
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+      ></video>
     </div>
-  )
+  );
 }
 
-export default Video
+export default Video;
